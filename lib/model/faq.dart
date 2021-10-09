d905c5ab59cc122472f0ba0012c0a13581811f0e
Aman Kumar <thisisamank@gmail.com>
2021-10-10 02:02:57 +0530

    fix: id added

--- lib/model/faq.dart
+++ lib/model/faq.dart
@@ -2,25 +2,31 @@ import 'dart:convert';
 
 class FaqModel {
   FaqModel({
+    required this.id,
     required this.title,
     required this.description,
   });
   factory FaqModel.fromMap(Map<String, dynamic> map) {
     return FaqModel(
-      title: map['title'] as String,
-      description: map['description'] as String,
+      id: map['id'].toString(),
+      title: map['title'].toString(),
+      description: map['description'].toString(),
     );
   }
   factory FaqModel.fromJson(String source) =>
       FaqModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  final String id;
   final String title;
   final String description;
 
   FaqModel copyWith({
+    String? id,
     String? title,
     String? description,
   }) {
     return FaqModel(
+      id: id ?? this.id,
       title: title ?? this.title,
       description: description ?? this.description,
     );
@@ -28,6 +34,7 @@ class FaqModel {
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'title': title,
       'description': description,
     };
@@ -36,17 +43,19 @@ class FaqModel {
   String toJson() => json.encode(toMap());
 
   @override
-  String toString() => 'FaqModel(title: $title, description: $description)';
+  String toString() =>
+      'FaqModel(id: $id, title: $title, description: $description)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
     return other is FaqModel &&
+        other.id == id &&
         other.title == title &&
         other.description == description;
   }
 
   @override
-  int get hashCode => title.hashCode ^ description.hashCode;
+  int get hashCode => id.hashCode ^ title.hashCode ^ description.hashCode;
 }
